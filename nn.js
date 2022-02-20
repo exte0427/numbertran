3699ff5971eb2160bd9ecd4d56abd184d1c92e31
ㄷㅌ <62054524+exte0427@users.noreply.github.com>
2022-02-20 20:44:35 +0900

    Update nn.js

--- nn.js
+++ nn.js
@@ -1,77 +1,36 @@
-const numbers=["","일","이","삼","사","오","육","칠","팔","구"]
-const 영 = 0;
-const 일 = 1;
-const 이 = 2;
-const 삼 = 3;
-const 사 = 4;
-const 오 = 5;
-const 육 = 6;
-const 칠 = 7;
-const 팔 = 8;
-const 구 = 9;
-let z=0;
-let 진척도=0;
-let str="";
-let strNUM="";
-for(let h=0;h<10;h++){
-  for(let k=0;k<10;k++){
-    for(let i=0;i<10;i++){
-      let num=numbers[i];
-      if(i==1){num="";}
-      for(let j=0;j<10;j++){
-        if(h==0){
-          if(k!=0){
-            if(i!=0){
-              eval(`var ${numbers[k]}백${num}십${numbers[j]} = ${k+""+i+""+j}`);
-              str=`${numbers[k]}백${num}십${numbers[j]}`;
-              strNUM=`${k+""+i+""+j}`;
-            }
-            else{
-              eval(`var ${numbers[k]}백${numbers[j]} = ${k+""+i+""+j}`);
-              str=`${numbers[k]}백${numbers[j]}`;
-              strNUM=`${k+""+i+""+j}`;
-            }
-          }
-          else{
-            eval(`var ${num}십${numbers[j]} = ${i+""+j}`);
-            str=`${num}십${numbers[j]}`;
-            strNUM=`${i+""+j}`;
-          }
-        }
-        else{
-          if(k!=0){
-            if(i!=0){
-              eval(`var ${numbers[h]}천${numbers[k]}백${num}십${numbers[j]} = ${h+""+k+""+i+""+j}`);
-              str=`${numbers[h]}천${numbers[k]}백${num}십${numbers[j]}`;
-              strNUM=`${h+""+k+""+i+""+j}`;
-            }
-            else{
-              eval(`var ${numbers[h]}천${numbers[k]}백${numbers[j]} = ${h+""+k+""+i+""+j}`);
-              str=`${numbers[h]}천${numbers[k]}백${numbers[j]}`;
-              strNUM=`${h+""+k+""+i+""+j}`;
-            }
-          }
-          else{
-            if(num!=0){
-              eval(`var ${numbers[h]}천${num}십${numbers[j]} = ${h+"0"+i+""+j}`);
-              str=`${numbers[h]}천${num}십${numbers[j]}`;
-              strNUM=`${h+"0"+i+""+j}`;
-            }
-            else{
-              eval(`var ${numbers[h]}천${numbers[j]} = ${h+"00"+j}`);
-              str=`${numbers[h]}천${numbers[j]}`;
-              strNUM=`${h+"00"+j}`;
-            }
-          }
-        }
-      }
-    }
-  }
-  console.clear();
-  진척도++;
-  let ssstr="";
-  for(let i=0;i<진척도;i++){ssstr=ssstr+"■"}
-  for(let i=ssstr.length;i<10;i++){ssstr=ssstr+"□"}
-  console.log("|"+ssstr);
-  console.log("└"+진척도+"/10 : "+str);
+'unsafe-eval'
+'self'
+'unsafe-inline'
+
+const 숫자 = ["", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"];
+const 단위 = ["십", "백", "천", "만"];
+
+const 만들어진수 = [[]];
+
+for (let 아이 = 0; 아이 < 10; 아이++) {
+    Function(`${숫자[아이] == "" ? "영" : 숫자[아이]}=${아이}`)();
+    만들어진수[0].push({ 네임: 숫자[아이], 넘버: 아이 });
 }
+
+for (let 아이 = 0; 아이 < 단위.length; 아이++) {
+    const 지금단위 = Math.pow(10, 아이 + 1);
+    만들어진수.push([]);
+    for (let 제이 = 1; 제이 < 10; 제이++) {
+        for (let 케이 = 0; 케이 <= 만들어진수.length - 2; 케이++) {
+            만들어진수[케이].forEach(수 => {
+                const 네임 = `${숫자[제이] == "일" ? "" : 숫자[제이]}${단위[아이]}${수.네임}`;
+                const 넘버 = 지금단위 * (제이) + 수.넘버;
+
+                Function(`${네임}=${넘버}`)();
+                만들어진수[만들어진수.length - 1].push({ 네임: 네임, 넘버: 넘버 });
+
+                if (넘버 % 3000 == 0) {
+                    console.clear();
+                    console.log(`${(넘버 * 100) / (99999)}% loaded`);
+                }
+            });
+        }
+    }
+}
+console.clear();
+console.log(`100% loaded`);
